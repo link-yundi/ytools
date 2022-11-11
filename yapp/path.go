68c665bb195d466f30801c51e57ed1a143552647
yundi <yundi.xxii@outlook.com>
2022-11-11 15:02:25 +0800

    app 路径规范

--- yapp/path.go
+++ yapp/path.go
@@ -16,34 +16,35 @@ Created on 2022-11-11 14:21
 
 // ========================== y 项目目录相关 ==========================
 var (
-	AppName      string
-	UserHomePath string // 用户家目录，绝对路径，由调用者设置
-	AppDir       string // app 目录 UserHomePath/applications/app-name
-	LogDir       string // 日志文件目录 UserHomePath/var/app-name/log
-	ConfDir      string // 配置文件目录路径 UserHomePath/etc/app-name/conf
+	AppName string
+	HomeDir string // 用户家目录，绝对路径，由调用者设置
+	AppDir  string // app 目录 HomeDir/applications/app-name
+	LogDir  string // 日志文件目录 HomeDir/var/app-name/log
+	ConfDir string // 配置文件目录路径 HomeDir/etc/app-name/conf
 )
 
 // app 初始化
 func InitApp(userHomePath, appname string) error {
 	var err error
 	AppName = appname
-	UserHomePath = userHomePath
-	AppDir = path.Join(UserHomePath, "applications", AppName)
-	LogDir = path.Join(UserHomePath, "var", AppName, "log")
-	ConfDir = path.Join(UserHomePath, "etc", AppName, "conf")
-	for _, dir := range []string{UserHomePath, AppDir, LogDir, ConfDir} {
+	HomeDir = userHomePath
+	AppDir = path.Join(HomeDir, "applications", AppName)
+	LogDir = path.Join(HomeDir, "var", AppName, "log")
+	ConfDir = path.Join(HomeDir, "etc", AppName, "conf")
+	for _, dir := range []string{HomeDir, AppDir, LogDir, ConfDir} {
 		err = ypath.MkDirs(dir)
 		if err != nil {
 			return err
 		}
 	}
+	Info()
 	return nil
 }
 
 // 打印app相关目录
 func Info() {
 	ylog.Info("AppName:", AppName)
-	ylog.Info("UserHomePath:", UserHomePath)
+	ylog.Info("HomeDir:", HomeDir)
 	ylog.Info("AppDir:", AppDir)
 	ylog.Info("LogDir:", LogDir)
 	ylog.Info("ConfDir:", ConfDir)
